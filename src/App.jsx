f6e8536b3f530d2e5ced88ab19387b8314c34fc2
vivekraut78 <vivekraut7707@gmail.com>
2026-06-29 08:43:37 +0530

    fix: correct Navbar import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/NavBar'
-import Hero from './components/Hero'
-import Projects from './components/Projects'
-import Skills from './components/Skills'
-import Contact from './components/Contact'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
 
-function App() 
-{
+function App() {
   return (
     <>
       <div>
-        <Navbar/>
-        <Hero/>
+        <Navbar />
+        <Hero />
         <Projects />
-        <Skills/>
-        <Contact/>
+        <Skills />
+        <Contact />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
